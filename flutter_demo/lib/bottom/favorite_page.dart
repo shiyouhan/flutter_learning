830d9f565eb64fcbe0b84429c37439f7a7ee9d0c
施友涵 <2470752581@qq.com>
2022-07-04 15:50:01 +0800

    feat: flutter组件学习（4）

---
+++ flutter_demo/lib/bottom/favorite_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/page/stateful_widget_page.dart';
+import 'package:flutter_demo/page/stateless_widget_page.dart';
+
+class FavoritePage extends StatefulWidget {
+  const FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
